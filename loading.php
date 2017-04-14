102d9296417bcc71eebc39cfd99001bcdcd8ec62
Abdul-Hamid Abdulai <oliham39@gmail.com>
2017-04-14 14:43:26 -0700

    deplicate

--- loading.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header( "refresh:5;url= staff_login.php" ); 
-    echo "<center><h1 style='color:red'>
-    Account created succussfully.
-    Now we would be redirecting you shortly to the staff login page.
-    </h1></center>";
-?>
